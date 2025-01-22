e6544a5589dcfe6fca9978c19d5d2b0e194aa693
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2025-01-22 19:02:50 +0100

    docs: Updated dependencies

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jonasw234/bedienungsanleitu.ng_downloader",
     packages=setuptools.find_packages(),
-    install_requires=["requests", "beautifulsoup4", "PIL"],
+    install_requires=["requests", "beautifulsoup4", "pdfkit", "wkhtmltopdf"],
     include_package_data=True,
     classifiers=[
         "Environment :: Console",
